565829c0d1e4827ceacfe493c2455acdd37d92ed
Vikas <44545660+vikasdo@users.noreply.github.com>
2021-03-31 08:15:11 +0000

    ds

--- operations.py
+++ operations.py
@@ -1,3 +1,6 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+
 
 def increment(event,context):
     '''
@@ -13,7 +16,10 @@ def square(event,context):
     response["square"]=event["increment"]*event["increment"]
     return response
 def decrement(event,context):
+    data = load_breast_cancer()
+    label_names = data['target_names']
     response=event
     response["decrement"]=event["square"]-1
+    response["label_names"]=label_names
     return response
 
